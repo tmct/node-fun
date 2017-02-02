3ad81c6519e59e22aae42ea4971c69db4a305d00
Tom McTiernan <thomasmctiernan@gmail.com>
2017-02-02 08:18:59 +0000

    Add pug view

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var express = require('express')
 var app = express()
 var uuidV4 = require('uuid/v4');
 
+app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
-  res.send('hello world, have a uuid: ' + uuidV4())
+  var message = 'hello world, have a uuid: ' + uuidV4()
+  res.render('index', { title: 'Hey', heading: 'It\'s demo time', message: message })
 })
 
 app.listen(3000, function () {
